0a1bd462e2e8e6e4914dc760e90a180a2b78b99c
Jeroen Mols <jeroen@jeroenmols.com>
2024-04-15 14:26:47 +0200

    migrate to alacritty toml file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ln -s ~/.dotfiles/zsh/config ~/.zshrc
-ln -s ~/.dotfiles/alacritty/config.yml ~/.alacritty.yml
+ln -s ~/.dotfiles/alacritty/config.toml ~/.alacritty.toml
 ln -s ~/.dotfiles/tmux/config ~/.tmux.conf
